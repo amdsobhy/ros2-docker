06edd88b83b4d5b30b56b9593ea74af3576e3e9a
Jeffery Li <jefli@blackberry.com>
2021-10-22 18:09:06 -0400

    Added more bind mounts to docker container creation script

---
+++ docker-create-container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Please run the script with the name of the distribution as an argument e.g: ./docker-build-image.sh foxy"
+	exit 1
+fi
+
+docker run -it -d \
+  --net=host \
+  --privileged \
+  -v ~/.vimrc:$HOME/.vimrc \
+  -v ~/.ssh:$HOME/.ssh \
+  -v ~/.qnx:$HOME/.qnx \
+  -v ~/shared:$HOME/shared \
+  -v $HOME/.qnx:$HOME/.qnx \
+  "qnxros2_$1:latest" /bin/bash
